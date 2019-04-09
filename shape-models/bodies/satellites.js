207b4381b8b4120b1b0f2ed819fcd91f1788273f
Conor Kingston <cmkingston21@me.com>
2019-04-09 11:13:53 -0700

    fixed prometheus usdz path

--- shape-models/bodies/satellites.js
+++ shape-models/bodies/satellites.js
@@ -142,7 +142,7 @@ let Satellites = function() {
                 saturnDataset(nullset),
                 stookDataset({
                     objPath: 'files/SATURN_SMALL_MOONS/prometheus/prometheus_30k_plt.tab.obj',
-                    usdzPath: 'files/SATURN_SMALL_MOONS/prometheus/prometheus_30k_plt.tab.usdz',
+                    usdzPath: 'files/SATURN_SMALL_MOONS/prometheus/prometheus_30k_plt.usdz',
                     preview: 'previews/prometheus_30k_plt.png'
                 })
             ]
